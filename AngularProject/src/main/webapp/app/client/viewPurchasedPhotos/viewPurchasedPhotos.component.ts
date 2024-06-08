1a2e811b9af6cb68fe5264da0c25f6ddf39ea84c
David_Oliveira_Magalhaes <1201237@isep.ipp.pt>
2024-06-08 21:53:23 +0100

    Implement View Purchased Photos

--- AngularProject/src/main/webapp/app/client/viewPurchasedPhotos/viewPurchasedPhotos.component.ts
+++ AngularProject/src/main/webapp/app/client/viewPurchasedPhotos/viewPurchasedPhotos.component.ts
@@ -1,43 +1,117 @@
-import { Component, inject, signal, OnInit } from '@angular/core';
-import {Router, RouterModule} from '@angular/router';
+import { Component, inject, signal, OnInit, NgZone } from '@angular/core';
+import { ActivatedRoute, Data, ParamMap, Router, RouterModule } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+import { sortStateSignal, SortDirective, SortByDirective, type SortState, SortService } from 'app/shared/sort';
+import { DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe } from 'app/shared/date';
+import { IPhotoMySuffix } from 'app/entities/photo-my-suffix/photo-my-suffix.model';
+import { EntityArrayResponseType, PhotoMySuffixService } from 'app/entities/photo-my-suffix/service/photo-my-suffix.service';
+import { SORT, ITEM_DELETED_EVENT, DEFAULT_SORT_DATA } from 'app/config/navigation.constants';
+import { DataUtils } from 'app/core/util/data-util.service';
 import SharedModule from 'app/shared/shared.module';
-import { LoginService } from 'app/login/login.service';
-import { AccountService } from 'app/core/auth/account.service';
-import { Account } from 'app/core/auth/account.model';
 import {FormsModule} from "@angular/forms";
-import {Observable} from "rxjs";
+import { combineLatest, filter, Observable, Subscription, tap } from "rxjs";
 
 @Component({
   standalone: true,
-  selector: 'jhi-home',
+  selector: 'jhi-photo-my-suffix',
   templateUrl: './viewPurchasedPhotos.component.html',
-  styleUrl: './viewPurchasedPhotos.component.scss',
-  imports: [SharedModule, RouterModule, FormsModule],
+  imports: [SharedModule,
+            RouterModule,
+            FormsModule,
+            SortDirective,
+            SortByDirective,
+            DurationPipe,
+            FormatMediumDatetimePipe,
+            FormatMediumDatePipe],
 })
 export default class ViewPurchasedPhotosComponent implements OnInit {
-  account = signal<Account | null>(null);
-  portfolioName : string = '';
-  private accountService = inject(AccountService);
-  private loginService = inject(LoginService);
-  private router = inject(Router);
+  subscription: Subscription | null = null;
+  photos?: IPhotoMySuffix[];
+  isLoading = false;
+
+  sortState = sortStateSignal({});
+
+  public router = inject(Router);
+  protected photoService = inject(PhotoMySuffixService);
+  protected activatedRoute = inject(ActivatedRoute);
+  protected sortService = inject(SortService);
+  protected dataUtils = inject(DataUtils);
+  protected modalService = inject(NgbModal);
+  protected ngZone = inject(NgZone);
+
+  trackId = (_index: number, item: IPhotoMySuffix): number => this.photoService.getPhotoMySuffixIdentifier(item);
 
   ngOnInit(): void {
-    this.accountService.identity().subscribe(account => this.account.set(account));
+    this.subscription = combineLatest([this.activatedRoute.queryParamMap, this.activatedRoute.data])
+      .pipe(
+        tap(([params, data]) => this.fillComponentAttributeFromRoute(params, data)),
+        tap(() => {
+          if (!this.photos || this.photos.length === 0) {
+            this.load();
+          }
+        }),
+      )
+      .subscribe();
   }
 
-  login(): void {
-    this.loginService.login();
+  byteSize(base64String: string): string {
+    return this.dataUtils.byteSize(base64String);
   }
-  onSubmit(): void {
-    this.accountService.identity().subscribe(account => {
-      console.log("boas");
-      if (account) {
-        console.log(this.portfolioName);
-        console.log(account.login);
-        console.log(account.email);
 
-      }
+  openFile(base64String: string, contentType: string | null | undefined): void {
+    return this.dataUtils.openFile(base64String, contentType);
+  }
+
+  load(): void {
+    this.queryBackend().subscribe({
+      next: (res: EntityArrayResponseType) => {
+        this.onResponseSuccess(res);
+      },
     });
   }
+
+  navigateToWithComponentValues(event: SortState): void {
+    this.handleNavigation(event);
+  }
+
+  protected fillComponentAttributeFromRoute(params: ParamMap, data: Data): void {
+    this.sortState.set(this.sortService.parseSortParam(params.get(SORT) ?? data[DEFAULT_SORT_DATA]));
+  }
+
+  protected onResponseSuccess(response: EntityArrayResponseType): void {
+    const dataFromBody = this.fillComponentAttributesFromResponseBody(response.body);
+    this.photos = this.refineData(dataFromBody);
+  }
+
+  protected refineData(data: IPhotoMySuffix[]): IPhotoMySuffix[] {
+    const { predicate, order } = this.sortState();
+    return predicate && order ? data.sort(this.sortService.startSort({ predicate, order })) : data;
+  }
+
+  protected fillComponentAttributesFromResponseBody(data: IPhotoMySuffix[] | null): IPhotoMySuffix[] {
+    return data ?? [];
+  }
+
+  protected queryBackend(): Observable<EntityArrayResponseType> {
+    this.isLoading = true;
+    const queryObject: any = {
+      sort: this.sortService.buildSortParam(this.sortState()),
+    };
+    return this.photoService.query(queryObject).pipe(tap(() => (this.isLoading = false)));
+  }
+
+  protected handleNavigation(sortState: SortState): void {
+    const queryParamsObj = {
+      sort: this.sortService.buildSortParam(sortState),
+    };
+
+    this.ngZone.run(() => {
+      this.router.navigate(['./'], {
+        relativeTo: this.activatedRoute,
+        queryParams: queryParamsObj,
+      });
+    });
+  }
+
 }
